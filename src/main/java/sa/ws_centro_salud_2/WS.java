a0b69c71464cb7d2f601b3edcb229bd745e76729
scxal <scxal@scxal-Aspire-E5-571>
2018-04-26 18:26:06 -0600

    1.0

--- src/main/java/sa/ws_centro_salud_2/WS.java
+++ src/main/java/sa/ws_centro_salud_2/WS.java
@@ -13,6 +13,7 @@ import java.sql.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
+import java.util.Date;
 
 /**
  *
@@ -25,7 +26,7 @@ public class WS {
      * This is a sample web service operation
      */
     
-    final String bd = "jdbc:mysql://35.196.79.167:3306/centro_salud";
+    final String bd = "jdbc:mysql://sql2.freemysqlhosting.net:3306/sql2232532";
     //final String bd = "jdbc:mysql://35.185.46.128:3306/centro_salud";
     
     @WebMethod(operationName = "IngresoPacienteCS2")
@@ -262,5 +263,169 @@ public class WS {
         return 1;
     }
     
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "TrasladoPaciente")
+    public String TrasladoPaciente(@WebParam(name = "informacion_traslado") String informacion_traslado) {
+        JSONObject obj = new JSONObject(informacion_traslado);
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            Connection con=DriverManager.getConnection(  
+            bd,"sql2232532","cR8%xR7!");  
+            Statement stmt=con.createStatement();
+                String query = "";
+                String query1 = "";
+                DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                Date date = new Date();
+                System.out.println(dateFormat.format(date));
+                query += "insert into TRASLADO_PACIENTE (FECHA,DPI,Origen,Destino)values("
+                        +"STR_TO_DATE('"+dateFormat.format(date)+"', '%d/%m/%Y'),"
+                        +obj.getString("Paciente")+","
+                        +obj.getString("Origen")+","
+                        +obj.getString("Destino")+");";
+                if(obj.getString("Destino").equals("2")){
+                    query1 += "update PACIENTE set Estado=1 where DPI="+obj.getString("Paciente")+";";
+                }else{
+                    query1 += "update PACIENTE set Estado=0 where DPI="+obj.getString("Paciente")+";";
+                }
+                PreparedStatement ps = con.prepareStatement(query);
+                ps.execute();
+                PreparedStatement ps1 = con.prepareStatement(query1);
+                ps1.execute();
+            con.close(); 
+            }catch(Exception e){ 
+                System.out.println(e);
+                return "{ \"Exito\":\"0\",\"Error\":\""+e.toString()+"\"}";
+            }  
+        
+        return "{ \"Exito\":\"1\",\"Error\":\"\"}";
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "HistorialPaciente")
+    public String HistorialPaciente(@WebParam(name = "DPI") String DPI) {
+        try{  
+            System.out.println(DPI);
+            DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
+            Date date = new Date();
+            JSONObject ent = new JSONObject(DPI);
+            //return "alguna estupidez";
+            System.out.println(dateFormat.format(date));
+            Class.forName("com.mysql.jdbc.Driver");  
+            Connection con=DriverManager.getConnection(  
+            bd,"sql2232532","cR8%xR7!");  
+            Statement stmt=con.createStatement();
+            ResultSet resultSet1 = stmt.executeQuery("select * from PACIENTE where DPI=\""+ent.getString("DPI")+"\";");
+            Date date1 = new Date();
+            System.out.println(dateFormat.format(date1));
+            org.json.simple.JSONArray historial = new org.json.simple.JSONArray();
+            JSONObject obj = new JSONObject();
+            while(resultSet1.next()){
+                obj.put("dpi", resultSet1.getString("DPI"));
+                obj.put("nombre", resultSet1.getString("Nombre"));
+                obj.put("apellido", resultSet1.getString("Apellido"));
+                obj.put("celular", resultSet1.getString("Celular"));
+                obj.put("telefono", resultSet1.getString("Télefono"));
+            }
+            ResultSet resultSet = stmt.executeQuery("select * from PACIENTE,CITA,ATENCION_MEDICA,DIAGNOSTICO where PACIENTE.DPI=\""+ent.getString("DPI")+"\" and PACIENTE.idPACIENTE=CITA.PACIENTE and CITA.idCITA=ATENCION_MEDICA.CITA and ATENCION_MEDICA.idATENCION_MEDICA=DIAGNOSTICO.idDIAGNOSTICO;");
+            while(resultSet.next()){
+                JSONObject obj1 = new JSONObject();
+                obj1.put("fecha", resultSet.getString("ATENCION_MEDICA.Fecha"));
+                obj1.put("motivo", resultSet.getString("ATENCION_MEDICA.Motivo"));
+                obj1.put("diagnostico", resultSet.getString("DIAGNOSTICO.Enfermedad"));
+                historial.add(obj1);
+            }
+            ResultSet resultSet2 = stmt.executeQuery("select * from TRASLADO_PACIENTE where DPI=\""+ent.getString("DPI")+"\";");
+            org.json.simple.JSONArray traslados = new org.json.simple.JSONArray();
+            while(resultSet2.next()){
+                JSONObject obj1 = new JSONObject();
+                obj1.put("fecha", resultSet2.getString("Fecha"));
+                obj1.put("origen", resultSet2.getString("Origen"));
+                obj1.put("destino", resultSet2.getString("Destino"));
+                traslados.add(obj1);
+            }
+            obj.put("historial",historial);
+            obj.put("traslados", traslados);
+            System.out.println(obj.toString());
+            return obj.toString();
+            }catch(Exception e){ 
+                System.out.println(e);
+                return "{}";
+            }
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "ReporteMorbilidad")
+    public String ReporteMorbilidad() {
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            Connection con=DriverManager.getConnection(  
+            bd,"sql2232532","cR8%xR7!");  
+            Statement stmt=con.createStatement();
+            ResultSet resultSet = stmt.executeQuery("SELECT QRY.* FROM (\n" +
+"	(select D.idDIAGNOSTICO as 'ID', D.ENFERMEDAD as 'Nombre', \n" +
+"	sum( case when timestampdiff(year, P.Fecha_Nac, curdate()) <= 5 then 1 else 0 END) as '0-5 años',\n" +
+"	sum( case when timestampdiff(year, P.Fecha_Nac, curdate()) >= 6 and \n" +
+"		timestampdiff(year, P.Fecha_Nac, curdate()) <= 15 then 1 else 0 END) as '6-15 años',\n" +
+"	sum( case when timestampdiff(year, P.Fecha_Nac, curdate()) >= 16 and \n" +
+"		timestampdiff(year, P.Fecha_Nac, curdate()) <= 21 then 1 else 0 END) as '16-21 años',\n" +
+"	sum( case when timestampdiff(year, P.Fecha_Nac, curdate()) >= 22 and \n" +
+"		timestampdiff(year, P.Fecha_Nac, curdate()) <= 45 then 1 else 0 END) as '22-45 años',\n" +
+"	sum( case when timestampdiff(year, P.Fecha_Nac, curdate()) >= 46 and \n" +
+"		timestampdiff(year, P.Fecha_Nac, curdate()) <= 60 then 1 else 0 END) as '46-60 años',\n" +
+"	sum( case when timestampdiff(year, P.Fecha_Nac, curdate()) >= 61 then 1 else 0 END) as '>= 61 años',\n" +
+"	sum( case when timestampdiff(year, P.Fecha_Nac, curdate()) then 1 else 0 END) as 'Total'\n" +
+"	from ATENCION_MEDICA AM, DIAGNOSTICO D, CITA C, PACIENTE P\n" +
+"	where AM.DIAGNOSTICO = D.idDIAGNOSTICO and AM.CITA = C.idCITA and C.PACIENTE = P.idPACIENTE\n" +
+"	group by D.ENFERMEDAD)\n" +
+"    \n" +
+"	UNION\n" +
+"    \n" +
+"    (select 999 as 'ID', 'Total', SUM(SUB.e5), SUM(SUB.e15), SUM(SUB.e21), SUM(SUB.e45), SUM(SUB.e60), SUM(SUB.e61), SUM(SUB.etotal) \n" +
+"    from (\n" +
+"		select D.Codigo, D.ENFERMEDAD, \n" +
+"		sum( case when timestampdiff(year, P.Fecha_Nac, curdate()) <= 5 then 1 else 0 END) as 'e5',\n" +
+"		sum( case when timestampdiff(year, P.Fecha_Nac, curdate()) >= 6 and \n" +
+"			timestampdiff(year, P.Fecha_Nac, curdate()) <= 15 then 1 else 0 END) as 'e15',\n" +
+"		sum( case when timestampdiff(year, P.Fecha_Nac, curdate()) >= 16 and \n" +
+"			timestampdiff(year, P.Fecha_Nac, curdate()) <= 21 then 1 else 0 END) as 'e21',\n" +
+"		sum( case when timestampdiff(year, P.Fecha_Nac, curdate()) >= 22 and \n" +
+"			timestampdiff(year, P.Fecha_Nac, curdate()) <= 45 then 1 else 0 END) as 'e45',\n" +
+"		sum( case when timestampdiff(year, P.Fecha_Nac, curdate()) >= 46 and \n" +
+"			timestampdiff(year, P.Fecha_Nac, curdate()) <= 60 then 1 else 0 END) as 'e60',\n" +
+"		sum( case when timestampdiff(year, P.Fecha_Nac, curdate()) >= 61 then 1 else 0 END) as 'e61',\n" +
+"		sum( case when timestampdiff(year, P.Fecha_Nac, curdate()) then 1 else 0 END) as 'etotal'\n" +
+"		from ATENCION_MEDICA AM, DIAGNOSTICO D, CITA C, PACIENTE P\n" +
+"		where AM.DIAGNOSTICO = D.idDIAGNOSTICO and AM.CITA = C.idCITA and C.PACIENTE = P.idPACIENTE\n" +
+"		group by D.ENFERMEDAD\n" +
+"	) as SUB)\n" +
+") AS QRY \n" +
+"ORDER BY QRY.ID;");
+            org.json.simple.JSONArray diagnosticos = new org.json.simple.JSONArray();
+            while(resultSet.next()){
+                JSONObject diagnostico = new JSONObject();
+                diagnostico.put("diagnostico",resultSet.getString("ID"));
+                diagnostico.put("nombre",resultSet.getString("Nombre"));
+                diagnostico.put("rango1",resultSet.getString("0-5 años"));
+                diagnostico.put("rango2",resultSet.getString("6-15 años"));
+                diagnostico.put("rango3",resultSet.getString("22-45 años"));
+                diagnostico.put("rango4",resultSet.getString("46-60 años"));
+                diagnostico.put("rango5",resultSet.getString(">= 61 años"));
+                diagnostico.put("total",resultSet.getString("total"));
+                diagnosticos.add(diagnostico);
+            }
+            JSONObject obj = new JSONObject();
+            obj.put("despacho",diagnosticos);
+            return obj.toString();
+            }catch(Exception e){ 
+                System.out.println(e);
+                return "{}";
+            }
+    }
     
 }
